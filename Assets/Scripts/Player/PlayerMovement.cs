8772cbda422671f9f1669839f44cd88ca781b015
nboisclair64 <59835675+nboisclair64@users.noreply.github.com>
2023-11-03 10:54:49 -0400

    Update PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,77 +10,45 @@ public class PlayerMovement : MonoBehaviour
     public Transform groundCheck;
     public LayerMask groundObjects;
     private Rigidbody2D rb;
-
+    
     private bool facingRight = true;
     private float moveDirection;
-    private bool isJumping = false;
     int jumpCount = 0;
     public GameObject isGround;
     public float checkRadius;
-    public void Awake()
-    {
+    private void Awake(){
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    public void Update()
+    void Update()
     {
         ProcessInputs();
         Animate();
     }
-<<<<<<< HEAD:Assets/Scripts/Player/PlayerMovement.cs
-    public void FixedUpdate()
-    {
-        isGround = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
-=======
     private void FixedUpdate(){
->>>>>>> main:Assets/Scripts/PlayerMovement.cs
         Move();
     }
-    private void Move()
-    {
+    private void Move(){
         rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
-<<<<<<< HEAD:Assets/Scripts/Player/PlayerMovement.cs
-
-        if (isJumping)
-        {
-            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
-            isJumping = false;
-        }
-
-=======
     }
     void Jump(){
             rb.AddForce(new Vector2(0f, jumpForce));
->>>>>>> main:Assets/Scripts/PlayerMovement.cs
     }
-    private void Animate()
-    {
-        if (moveDirection > 0 && !facingRight)
-        {
+    private void Animate(){
+        if(moveDirection>0 && !facingRight){
             FlipCharacter();
         }
-        else if (moveDirection < 0 && facingRight)
-        {
+        else if (moveDirection<0 && facingRight){
             FlipCharacter();
         }
     }
-    private void FlipCharacter()
-    {
+    private void FlipCharacter(){
         facingRight = !facingRight;
-        transform.Rotate(0f, 180f, 0f);
+        transform.Rotate(0f,180f,0f);
     }
-    private void ProcessInputs()
-    {
+    private void ProcessInputs(){
         moveDirection = Input.GetAxis("Horizontal");
-<<<<<<< HEAD:Assets/Scripts/Player/PlayerMovement.cs
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGround)
-        {
-            isJumping = true;
-        }
-
-=======
         
         if(Input.GetButtonDown("Jump")){
             
@@ -104,6 +72,5 @@ public class PlayerMovement : MonoBehaviour
         if(c.gameObject.layer==6){
             jumpCount = 0;
         }
->>>>>>> main:Assets/Scripts/PlayerMovement.cs
     }
 }
